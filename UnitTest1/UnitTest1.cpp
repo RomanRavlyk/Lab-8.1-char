ae97de7f0d38f82423fab39606f960af09bcb61c
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2023-11-16 12:14:45 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 8.1 char/Lab 8.1 char.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+
+        TEST_METHOD(FindCharTest)
+        {
+            char str[12] = "1,2,4,5,6,7";
+            int result = Count(str);
+            Assert::AreEqual(result, 5);
+        }
+    };
+}
